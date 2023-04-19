9d9cba953f1fc4a3a1c4c9ef92d191f6c1cc05a5
GV8738 <GV8738@zebra.com>
2023-04-19 13:16:52 -0400

    Bug in configuration solved

--- configuration.py
+++ configuration.py
@@ -28,7 +28,7 @@ def argparser():
     parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
     parser.add_argument('--momentum', type=float, default=0.9, help='Momentum for optimizer')
     parser.add_argument('--weight-decay', type=float, default=1e-4, help='Weight decay for optimizer')
-    parser.add_argument('--optimizer', type=str, default='sgd', help='Optimizer to use', choices=('sgd', 'adam'))
+    parser.add_argument('--optim', type=str, default='sgd', help='Optimizer to use', choices=('sgd', 'adam'))
     parser.add_argument('--parameters', type=str, default='normal', help='Model parameters to train', choices=('normal', 'special'))
     parser.add_argument('--evaluate', action='store_true', help='Evaluating on evaluation set')
     parser.add_argument('--resume', type=bool, default=False, help='Continuing from checkpoint')
