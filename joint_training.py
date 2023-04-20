d45657e844e985544b4650f719f55890214270d5
GV8738 <GV8738@zebra.com>
2023-04-20 16:18:16 -0400

    Printing messages simplified. Bugs Solved.

--- joint_training.py
+++ joint_training.py
@@ -14,6 +14,7 @@ def main(args):
         rank, current_device = dist_utils.dist_configuration(args)
     else:
         current_device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+        rank = 0
 
     # Creating Model
     if args.dataset in ['visda', 'office', 'imagenet']:
@@ -21,7 +22,7 @@ def main(args):
             weights = torch.load(args.root +'weights/NAME_OF_PRETRAINED_WEIGHTS.pth')
         else:
             weights = torch.load(args.root + 'weights/resnet50_imagenet.pth')
-            if args.dataset is not 'imagenet':
+            if args.dataset != 'imagenet':
                 del weights['fc.weight']
                 del weights['fc.bias']
     else:
@@ -31,10 +32,8 @@ def main(args):
             weights = None
     model = model_utils.create_model(args, augment=True, weights=weights).to(current_device)
     if args.distributed:
-        dist_utils.dist_message('model', rank, model=args.model)
         model = DDP(model, device_ids=[current_device], find_unused_parameters=True)
-    else:
-        utils.message('model', model=args.model)
+    utils.message('model', rank, model=args.model)
 
     # Model parameters and optimizer
     if args.parameters == 'special':
@@ -47,6 +46,8 @@ def main(args):
         optimizer = torch.optim.Adam(parameters, args.lr)
     if args.dataset in ['cifar10', 'cifar100']:
         scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=(150, 250), gamma=0.1)
+    else:
+        scheduler = None
 
     # Loading checkpoint
     if args.resume:
@@ -59,37 +60,29 @@ def main(args):
         if args.dataset in ['cifar10', 'cifar100']:
             scheduler.load_state_dict(checkpoint['scheduler'])
         args.start_epoch = checkpoint['start_epoch']
-        if args.distributed:
-            dist_utils.dist_message('checkpoint', rank, epoch=checkpoint['start_epoch'])
-        else:
-            utils.message('checkpoint', epoch=checkpoint['start_epoch'])
+        utils.message('checkpoint', rank, epoch=checkpoint['start_epoch'])
 
     # Generating dataloader
-    if args.distributed:
-        dist_utils.dist_message('data', rank, data=args.dataset)
-    trloader, tr_sampler, teloader, te_sampler = prepare_dataset.prepare_train_data(args)
+    utils.message('data', rank, dataset=args.dataset)
+    train_loader, train_sampler, val_loader, val_sampler = prepare_dataset.prepare_train_data(args)
 
     # Loss function
     crossentropy = torch.nn.CrossEntropyLoss()
     criterion = train_utils.CustomLoss(crossentropy=crossentropy)
 
     # Starting joint training
-    dist_utils.dist_message('metrics', rank)
+    utils.message('metrics', rank)
     for epoch in range(args.start_epoch, args.epochs):
-        tr_sampler.set_epoch(epoch)
-        te_sampler.set_epoch(epoch)
+        train_sampler.set_epoch(epoch)
+        val_sampler.set_epoch(epoch)
 
         # Training step
-        acc_train, loss_train, tr_epoch_time = train_utils.train(model, criterion, optimizer, trloader, augment=False, custom_forward=True)
+        acc_train, loss_train, tr_epoch_time = train_utils.train(model, current_device, criterion, optimizer, train_loader, augment=False, custom_forward=True)
 
         # Valuation step
-        acc_val, loss_val, val_epoch_time = train_utils.validate(model, criterion, trloader, augment=False, custom_forward=True)
+        acc_val, loss_val, val_epoch_time = train_utils.validate(model, current_device, criterion, val_loader, augment=False, custom_forward=True)
 
-        if args.distributed:
-            dist_utils.dist_message('epoch', rank, epoch=epoch, epochs=args.epochs, loss_train=loss_train, acc_train=acc_train, time_train=tr_epoch_time,
-                                                   loss_val=loss_val, acc_val=acc_val, time_val=val_epoch_time)
-        else:
-            utils.message('epoch', rank, loss_train=loss_train, acc_train=acc_train, time_train=tr_epoch_time,
+        utils.message('epoch', rank, epoch=epoch, epochs=args.epochs, loss_train=loss_train, acc_train=acc_train, time_train=tr_epoch_time,
                                                    loss_val=loss_val, acc_val=acc_val, time_val=val_epoch_time)
 
         # Saving checkpoint
@@ -100,12 +93,8 @@ def main(args):
                      'state_dict': model_state,
                      'optimizer': optimizer.state_dict(),
                      'scheduler': scheduler_state}
-            if args.distributed and rank == 0:
-                utils.save_checkpoint(state, args)
-            else:
-                utils.save_checkpoint(state, args)
-
-
+            utils.save_checkpoint(state, rank, 'joint', args)
+M
 if __name__=='__main__':
     args = configuration.argparser()
     main(args)
