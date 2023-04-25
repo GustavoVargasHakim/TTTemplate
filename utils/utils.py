d4dea1e4545d1847e5b7e69f2acf039fe70ff737
GV8738 <GV8738@zebra.com>
2023-04-25 10:57:20 -0400

    Execution info displaying improved inside utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,18 @@
 import torch
 
+#Returns the an integer code corresponding to the chosen layers (useful for TTT methods)
+def layer_codes(layers):
+    '''
+    Returns integer code for
+    :param layers: List of integers defining layers (e.g., [1,0,0,0] for layer 1)
+    :return: integer code to add to save_checkpoint (e.g., 1)
+    '''
+    layer_code = ''
+    for elem in layers:
+        layer_code += str(elem)
+    codes = {'1000':1, '0100':2, '0010':3, '0001':4, '1100':12, '0110':23, '0011':34, '1111':'all'}
+    return codes[layer_code]
+
 def save_checkpoint(args, state, rank, mode, **kwargs):
     if rank == 0:
         if mode == 'source':
@@ -22,15 +35,14 @@ def message(case, rank, checkpoint=None, **kwargs):
         elif case == 'checkpoint':
             print("Loaded checkpoint (from epoch {})".format(checkpoint['epoch']))
         elif case == 'metrics':
-            print('\t\tTrain Loss \t\t Train Accuracy (%) \t\t Train time (s) \t\t Val Loss \t\t Val Acccuracy (%) \t\t Val time (s)')
+            print('\t\tTrain Loss\t\tTrain Acc (%)\t\t Val Loss\t\t Val Acc (%)\t\tTime (s)')
         elif case == 'epoch':
             loss_train = kwargs['loss_train']
             acc_train = kwargs['acc_train']
-            time_train = kwargs['time_train']
             loss_val = kwargs['loss_val']
             acc_val = kwargs['acc_val']
-            time_val = kwargs['time_val']
             epoch = kwargs['epoch']
             epochs = kwargs['epochs']
+            time_epoch = kwargs['time_epoch']
             print(('Epoch %d/%d:' % (epoch, epochs)).ljust(24) +
-                   '%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f' % (loss_train,acc_train,time_train,loss_val,acc_val,time_val))
+                   '%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f' % (loss_train,acc_train,loss_val,acc_val,time_epoch))
